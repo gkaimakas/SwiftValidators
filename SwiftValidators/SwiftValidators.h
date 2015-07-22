9380dfc86c2c69f8524eb5a5cbc67a722f3e832a
gkaimakas <gkaimakas@gmail.com>
2015-07-22 19:30:47 +0300

    Initial Commit

---
+++ SwiftValidators/SwiftValidators.h
@@ -0,0 +1,19 @@
+//
+//  SwiftValidators.h
+//  SwiftValidators
+//
+//  Created by Γιώργος Καϊμακάς on 7/22/15.
+//  Copyright (c) 2015 Γιώργος Καϊμακάς. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftValidators.
+FOUNDATION_EXPORT double SwiftValidatorsVersionNumber;
+
+//! Project version string for SwiftValidators.
+FOUNDATION_EXPORT const unsigned char SwiftValidatorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftValidators/PublicHeader.h>
+
+
