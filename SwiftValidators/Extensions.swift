a97c8b44901992c926fa6a6afd095ea76e7a2ab5
gkaimakas <gkaimakas@gmail.com>
2016-05-10 12:54:10 +0300

    removed a warning bound for deprecation in Swift 3.0

--- SwiftValidators/Extensions.swift
+++ SwiftValidators/Extensions.swift
@@ -12,7 +12,7 @@ internal extension String {
     }
     
     internal subscript (r: Range<Int>) -> String {
-        return substringWithRange(Range(start: startIndex.advancedBy(r.startIndex), end: startIndex.advancedBy(r.endIndex)))
+        return substringWithRange(Range(startIndex.advancedBy(r.startIndex) ..< startIndex.advancedBy(r.endIndex)))
     }
 
     internal var length: Int {
